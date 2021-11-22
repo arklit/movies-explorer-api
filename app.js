bf311e1a125115ce561bbed8f879bbda280951a5
Artem <artemoutside@gmail.com>
2021-11-22 14:59:23 +0300

    test

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
+const { errors } = require('celebrate');
+const { dataMovies, PORT } = require('./utils/const');
+const limiter = require('./middlewares/limiter');
+const errorsHandler = require('./middlewares/error');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const router = require('./routes/index');
+
+require('dotenv').config();
+
+const app = express();
+
+const options = {
+  origin: [
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'http://api.kinofilm.nomoredomains.work',
+    'https://api.kinofilm.nomoredomains.work',
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+  credentials: true,
+};
+
+app.use(requestLogger);
+app.use(limiter);
+app.use(helmet());
+
+app.use('*', cors(options));
+app.use(cookieParser());
+app.use(express.json());
+app.use(router);
+app.use(errorLogger);
+app.use(errors());
+app.use(errorsHandler);
+
+mongoose.connect(dataMovies, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
---
+++ middlewares/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const UnauthorizedError = require('../errors/unauthorized-err');
+const messages = require('../errors/errorsMessages');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+
+module.exports = (req, res, next) => {
+  const token = req.cookies.jwt;
+
+  if (!token) {
+    throw new UnauthorizedError(messages.unauthorizeError);
+  }
+
+  let payload;
+
+  try {
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'super-strong-secret');
+    req.user = payload;
+    next();
+  } catch (err) {
+    throw new UnauthorizedError(messages.unauthorizeError);
+  }
+};
